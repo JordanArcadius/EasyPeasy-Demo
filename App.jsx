92e5b180c74ca5fa1c360ee62f0bc8607587f66a
JordanArcadius <jordanarcadius2001@gmail.com>
2025-04-09 21:08:33 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+const App = () => {
+  return <h1>Welcome to EasyPeasy FMCG Store</h1>
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
